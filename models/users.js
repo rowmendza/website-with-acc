a02f0243734056538535dd72fda36e108cb2bfa7
rowmendza <rowmendz@mail.sfsu.edu>
2015-11-15 07:18:52 +0000

    first commit

---
+++ models/users.js
@@ -0,0 +1,11 @@
+// models/user.js
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('User', {
+    id: String,
+    username: String,
+    password: String,
+    email: String,
+    firstName: String,
+    lastName: String
+});
